2abea26c63a1b2dcd16324762717a5bbcdf5e7a8
phihash <insharp0220@gmail.com>
2023-01-22 05:46:14 +0900

    Footer編集

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,10 @@ const Footer = () => {
 
   <footer class="mt-28 p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-800">
       <div class="sm:flex sm:items-center sm:justify-between">
-          <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
-              <BiSleepy className='text-4xl text-white mx-2'></BiSleepy>
+          <a href="https://twitter.com/phihash" class="flex items-center mb-4 sm:mb-0">
+              <FaTwitter className='text-2xl text-white mx-2'></FaTwitter>
               <Link to="/"> <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Quick</span></Link>
           </a>
-          <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-              <li>
-                  <a href="#" class="mr-4 hover:underline md:mr-6 "><FaTwitter className='text-2xl'></FaTwitter></a>
-              </li>
-          </ul>
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://phihash.net" class="hover:underline">phihash</a> Quick
